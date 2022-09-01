65552f890eb030f818cae3e21c3b0195c3a5c8e9
Bruno Bispo <bbispo.by@gmail.com>
2022-09-01 16:57:25 -0300

    feat: add home page ui and controller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
+  final RxList<Widget> children = [Text('Page 1'), Text('Page 2')].obs;
+  RxInt currentIndex = 0.obs;
 
   final count = 0.obs;
   @override
@@ -20,4 +23,8 @@ class HomeController extends GetxController {
   }
 
   void increment() => count.value++;
+
+  void onTap(int index) {
+    currentIndex.value = index;
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import '../widgets/bottom_nav_item.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return SafeArea(
+      child: Obx(
+        () => Scaffold(
+          body: controller.children[controller.currentIndex.value],
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: true,
+            currentIndex: controller.currentIndex.value,
+            items: [
+              bottomNavItem(Icons.play_arrow, "Listen"),
+              bottomNavItem(Icons.favorite, "Favorite"),
+            ],
+            onTap: (int index) {
+              controller.onTap(index);
+            },
+          ),
         ),
       ),
     );
